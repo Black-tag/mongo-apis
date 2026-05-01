bb7bd5e117c383df2b95fe6fe76f5b3095a1e441
Black-tag <unnianandunni007@gmail.com>
2026-05-01 04:06:12 +0000

    chnaged driver into asynciomotor again

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ MONGO_URI="mongodb://localhost:27017"
 DB_NAME="digicollect"
 
 app = FastAPI()
-client = AsyncMongoClient(MONGO_URI)
+client = AsyncIOMotorClient(MONGO_URI)
 db = client[DB_NAME]
 
 
